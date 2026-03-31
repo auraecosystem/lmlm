8cbc845088170b73777224d3b7280522b6163789
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-31 20:36:16 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,6 @@
-```bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi.llvms
+```bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi.llvms```
+ollama pull llama3.2
+echo "FROM llama3.2" >> Modelfile
+echo "SYSTEM You are a friendly assistant." >> Modelfile
+ollama create -f Modelfile lmlm/Lmkm
+ollama push lmlm/Lmkm
